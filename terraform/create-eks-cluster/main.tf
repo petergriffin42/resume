a6f122ba44277a465fbc31834192629f6bc0c822
Peter Griffin <pgriffwork@gmail.com>
2022-11-28 12:20:22 -0700

    Fixing format

--- terraform/create-eks-cluster/main.tf
+++ terraform/create-eks-cluster/main.tf
@@ -9,9 +9,9 @@ provider "kubernetes" {
 }
 
 provider "aws" {
-  region     = var.region
-#  access_key = var.access_key
-#  secret_key = var.secret_key
+  region = var.region
+  #  access_key = var.access_key
+  #  secret_key = var.secret_key
 }
 
 data "aws_availability_zones" "available" {}
