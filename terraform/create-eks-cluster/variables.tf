36aa664049e057182c5bfa0dddf790a7a076e50a
Peter Griffin <pgriffwork@gmail.com>
2022-11-28 12:26:46 -0700

    Commenting more variables

--- terraform/create-eks-cluster/variables.tf
+++ terraform/create-eks-cluster/variables.tf
@@ -4,14 +4,14 @@ variable "region" {
   default     = "us-west-2"
 }
 
-variable "access_key" {
-  description = "AWS access key"
-  type        = string
-  sensitive   = true
-}
+#variable "access_key" {
+#  description = "AWS access key"
+#  type        = string
+#  sensitive   = true
+#}
 
-variable "secret_key" {
-  description = "AWS secret key"
-  type        = string
-  sensitive   = true
-}
+#variable "secret_key" {
+#  description = "AWS secret key"
+#  type        = string
+#  sensitive   = true
+#}
